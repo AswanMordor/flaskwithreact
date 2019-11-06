353a64aee5326e9b252d68754c072a400dad65a0
Sam Wang <samuel.joshua.wang@gmail.com>
2019-11-06 17:15:58 -0600

    Google Authentication

---
+++ react-frontend/src/credentials/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDFfbMOd0rzQbt3bhnn7h-oauHQu2JuSVY",
+  authDomain: "fitfinder-3e49c.firebaseapp.com",
+  databaseURL: "https://fitfinder-3e49c.firebaseio.com",
+  projectId: "fitfinder-3e49c",
+  storageBucket: "fitfinder-3e49c.appspot.com",
+  messagingSenderId: "303945530497",
+  appId: "1:303945530497:web:9cb30e71e5efe8afbb6180"
+};
