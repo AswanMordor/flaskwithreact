129e2107a2136160b99dfedd36a7bc79cf5a6edb
Sam Wang <samuel.joshua.wang@gmail.com>
2019-11-06 17:15:58 -0600

    Google Authentication

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,56 +4,57 @@ import FindMyFit from "./findmyfit";
 import About from "./about";
 import "./css/App.css";
 import "./css/navbar.css";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import banner from "./img/header.png";
 // import banner from "./img/header2.png";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <div>
-            {/* <img src={require("/Users/nnsim/Documents/EE461L/FitFinder/site-2-react/fitfinder-react/src/header.png")} /> */}
-            <img src={banner} alt={"banner"} />
-          </div>
-          <div class="topnav">
-             <Link class="link" to="/about">
-              About
-            </Link>
+    // <div className="App">
+    //   <Router>
+    //     <div>
+    //       <div>
+    //         {/* <img src={require("/Users/nnsim/Documents/EE461L/FitFinder/site-2-react/fitfinder-react/src/header.png")} /> */}
+    //         <img src={banner} alt={"banner"} />
+    //       </div>
+    //       <div class="topnav">
+    //         <Link class="link" to="/about">
+    //           About
+    //         </Link>
 
-            <Link class="link" to="/findmyfit">
-              Find Your Fit
-            </Link>
+    //         <Link class="link" to="/findmyfit">
+    //           Find Your Fit
+    //         </Link>
 
-            <Link class="link" to="/createuser">
-              Create an Account
-            </Link>
+    //         <Link class="link" to="/createuser">
+    //           Create an Account
+    //         </Link>
 
-             <Link class="link" to="/login">
-              Login
-            </Link>
-          </div>
+    //         <Link class="link" to="/login">
+    //           Login
+    //         </Link>
+    //       </div>
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/findmyfit">
-              <FindFit />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/create/user">
-              <CreateUser />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    //       {/* A <Switch> looks through its children <Route>s and
+    //         renders the first one that matches the current URL. */}
+    //       <Switch>
+    //         <Route path="/about">
+    //           <About />
+    //         </Route>
+    //         <Route path="/findmyfit">
+    //           <FindFit />
+    //         </Route>
+    //         <Route path="/login">
+    //           <Login />
+    //         </Route>
+    //         <Route path="/create/user">
+    //           <CreateUser />
+    //         </Route>
+    //       </Switch>
+    //     </div>
+    //   </Router>
+    // </div>
+    <div>Hello World</div>
   );
 }
 
@@ -66,7 +67,7 @@ function Login() {
 }
 
 function about() {
-  return <About />
+  return <About />;
 }
 
 function FindFit() {
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "./css/index.css";
+import App from "./App";
+import Login from "./components/Login";
+import CreateUser from "./components/CreateUser";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as firebase from "firebase";
+import firebaseConfig from "./credentials/firebase.config";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp(firebaseConfig);
+export const AuthContext = React.createContext(null);
+
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/CreateUser">Create User</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route exact path="/Login" component={Login} />
+      <Route exact path="/CreateUser" component={CreateUser} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
