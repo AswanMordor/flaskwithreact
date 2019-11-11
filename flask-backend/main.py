e59514fb60a5b634901bd93b1ab1392448bffaf2
AzzyMordor <Darthneel@gmail.com>
2019-11-11 16:14:09 -0600

    formatting

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request
+
 app = flask.Flask(__name__)
 
 
@@ -7,12 +8,9 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template("index.html", token="Sucessful Flask Test")
 
+
 @app.route('/temp', methods=('GET', 'POST'))
 def tempCom():
-
-
-
-
     response = flask.jsonify({'res': "ERROR"})
     if request.method == 'POST':
         response = flask.jsonify({'res': 'POST REQUEST RECEIVED FROM SERVER'})
@@ -20,6 +18,7 @@ def tempCom():
         response = flask.jsonify({'res': 'GET REQUEST RECEIVED FROM SERVER'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
