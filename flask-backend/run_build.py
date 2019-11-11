a29b256c30a2303e4a731b34ff6b110e13631dc3
AzzyMordor <Darthneel@gmail.com>
2019-11-11 17:38:30 -0600

    added deploy script

--- flask-backend/run_build.py
+++ flask-backend/run_build.py
@@ -1,19 +1,23 @@
 import subprocess
+import webbrowser
 
-target_types = ["list", "build", "test", "deploy react", "deploy server local", "deploy dev local ", "deploy gcloud"]
+target_types = ["list", "build", "test", "deploy", "deploy dev", "deploy gcloud"]
 target = input("Enter a target: ")
 if target == "list":
     print("Target Types: \n", target_types)
 if target == "deploy":
-    print("Deploying frontend from /react-frontend")
-    print("This will NOT build the frontend for server deployment, use build or deploy server local instead")
-    print("Running npm start from /react-frontend")
-    npm_start = subprocess.run(['npm', 'start'], check=True, stdout=subprocess.PIPE, universal_newlines=True,
-                               shell=True)
-# run() returns a CompletedProcess object if it was successful
-# errors in the created process are raised here too
-process = subprocess.run(['echo', 'how are you?'], check=True, stdout=subprocess.PIPE, universal_newlines=True,
-                         shell=True)
-output = process.stdout
-
-print(output)
+    print("Deploying locally via python server from /react-frontend")
+    print("Running npm run build from /react-frontend")
+    npm_run_build = subprocess.run(['cd', '../react-frontend', '&&', 'npm', 'run', "build"], check=True,
+                                   stdout=subprocess.PIPE,
+                                   universal_newlines=True,
+                                   shell=True)
+    print(npm_run_build)
+    print("Running main.py from /flask-backend")
+    python_main = subprocess.run(['cd', '../flask-backend', '&&', 'python', 'main.py'], check=True,
+                                 stdout=subprocess.PIPE,
+                                 universal_newlines=True,
+                                 shell=True)
+    print(python_main)
+    webbroswer_address = "http://www.google.com"
+    webbrowser.open_new_tab(webbroswer_address)
