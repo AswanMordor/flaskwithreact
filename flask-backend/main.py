c561c1846643479f648a892ca7dc9c41d9a1216d
Andy Prevalsky <andyprevalsky@yahoo.com>
2019-11-11 15:23:19 -0600

    sample communication built

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,5 +1,5 @@
 import flask
-
+from flask import request
 app = flask.Flask(__name__)
 
 
@@ -7,6 +7,19 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template("index.html", token="Sucessful Flask Test")
 
+@app.route('/temp', methods=('GET', 'POST'))
+def tempCom():
+
+
+
+
+    response = flask.jsonify({'res': "ERROR"})
+    if request.method == 'POST':
+        response = flask.jsonify({'res': 'POST REQUEST RECEIVED FROM SERVER'})
+    if request.method == 'GET':
+        response = flask.jsonify({'res': 'GET REQUEST RECEIVED FROM SERVER'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-frontend/src/findmyfit.js
+++ react-frontend/src/findmyfit.js
@@ -4,13 +4,39 @@ import "./css/findmyfit.css";
 // import 'bootstrap/js/dist/button';
 import Results from "./item";
 
-class FindMyFit extends React.Component {
+const backendUrl = "http://127.0.0.1:5000/"
 
+
+class FindMyFit extends React.Component {
+  state = {requestText: ""}
   fileRef = React.createRef();
 
   uploadClick() {
     document.getElementById("uploadImg").click()
   }
+
+  tempGet() {
+    fetch(backendUrl+"temp", {
+      method: 'GET'
+    }).then(response => response.json().then(jresponse => {
+      console.log(jresponse)
+      this.setState({requestText: jresponse.res})
+    })).catch(() => {
+      console.log("ERROR")
+    })
+}
+
+  tempPost() {
+    fetch(backendUrl+"temp", {
+      method: 'POST'
+    }).then(response => response.json().then(jresponse => {
+      console.log(jresponse)
+      this.setState({requestText: jresponse.res})
+    })).catch(() => {
+      console.log("ERROR")
+    })
+  }
+
 
   state = {
     selectedFile: null
@@ -30,7 +56,10 @@ class FindMyFit extends React.Component {
         <SideNav />
         <div class="upload">
           <input id="uploadImg" type="file" onChange={this.fileUploadHandler} hidden/>
-          <button type="button" class="btn btn-outline-dark" onClick={this.uploadClick}>Upload</button>
+          <button type="button" class="btn btn-outline-dark" onClick={this.uploadClick.bind(this)}>Upload</button>
+          <button type="button" class="btn btn-outline-dark" onClick={this.tempGet.bind(this)}>SEND GET REQUEST</button>
+          <button type="button" class="btn btn-outline-dark" onClick={this.tempPost.bind(this)}>SEND POST REQUEST</button>
+          <button type="button" class="btn btn-outline-dark">{this.state.requestText}</button>
           {/* <img src={require(this.state.selectedFile)} /> */}
         </div>
 
