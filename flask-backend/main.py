0a58f9309b56210c46c77e708630529890964648
AzzyMordor <Darthneel@gmail.com>
2019-11-11 18:27:08 -0600

    reqs

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,18 +1,16 @@
 import flask
 from flask import request
-
-
-app = flask.Flask(__name__)
-
 from google.cloud import storage
 import uuid
 import requests
 from flask_cors import CORS
 import os
 
+app = flask.Flask(__name__)
 
 app = flask.Flask(__name__)
 CORS(app)
+
 
 @app.route('/')
 def index():
@@ -30,10 +28,10 @@ def tempCom():
     return response
 
 
-@app.route('/productSearch', methods=('GET' ,'POST'))
+@app.route('/productSearch', methods=('GET', 'POST'))
 def productSearch():
     bucket_name = "fitfinder-3e49c.appspot.com"
-    imageName=  str(uuid.uuid4())
+    imageName = str(uuid.uuid4())
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(imageName)
@@ -44,7 +42,7 @@ def productSearch():
     with open(imageName, "rb") as my_file:
         blob.upload_from_file(my_file)
     os.remove(imageName)
-    headers = { #TODO: get auth token dynamically
+    headers = {  # TODO: get auth token dynamically
         "Authorization": "Bearer ya29.c.Kl6wB0is7W_-Qn5tUHzdIQjcxlqhs4jz9TMchIn4vvfKIqai3dEiQcLj0vHMLvQKSn54mkbgRYQkkQh86Yy8Z8Q2wfQ06cJGmSMcHucwUndmLmxZ0wun_zjvL2tFO_CG",
         "Content-Type": "application/json"
     }
@@ -53,7 +51,7 @@ def productSearch():
             {
                 "image": {
                     "source": {
-                        "gcsImageUri": "gs://fitfinder-3e49c.appspot.com/"+imageName
+                        "gcsImageUri": "gs://fitfinder-3e49c.appspot.com/" + imageName
                     }
                 },
                 "features": [
@@ -73,7 +71,8 @@ def productSearch():
         ]
     }
     gcsResponse = requests.post('https://vision.googleapis.com/v1/images:annotate', json=payload, headers=headers)
-    results = [i["product"]["name"].split("/")[-1] for i in gcsResponse.json()["responses"][0]["productSearchResults"]["results"]] #parse names from gscResponse
+    results = [i["product"]["name"].split("/")[-1] for i in
+               gcsResponse.json()["responses"][0]["productSearchResults"]["results"]]  # parse names from gscResponse
     response = flask.jsonify({"results": results})
     return response
 
